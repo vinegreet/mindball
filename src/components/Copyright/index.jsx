8d2879ef339d082d7a5f09c06542acb27127cd82
vinegreet <vinegreet@gmail.com>
2018-07-17 13:22:17 +0300

    Rearrange sentences in Copyright; swap BgText and Bubbles

--- src/components/Copyright/index.jsx
+++ src/components/Copyright/index.jsx
@@ -6,8 +6,8 @@ export default function Copyright(props) {
   return (
     <div className={styles.Copyright} style={{ zIndex: props.zIndex, opacity: props.opacity }}>
         <div className={styles.text}>
-          <p>© {new Date().getFullYear()} Mindball Ukraine. All Rights Reserved.</p>
-          <p>Powered by Contentful</p>
+          <p>© {new Date().getFullYear()} Mindball Ukraine.</p>
+          <p>All Rights Reserved. Powered by Contentful.</p>
         </div>
         <div className={styles.idea}>
           <a href='http://ideaua.com/' target='_blank'>
